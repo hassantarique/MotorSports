30cd36c899d0c975939f592df44bdc6ba00f3c0c
M16A4PAPER <m16a4paper@gmail.com>
2024-12-15 12:41:51 +0500

    End of Module Two

--- C02-Initial/ConsoleDemo/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿try
-{
-    foreach (Worker worker in Workers.TestData)
-        Console.WriteLine(worker);
-}
-catch (Exception e)
-{
-    Console.WriteLine($"ERROR: {e.Message}");
-}
---
+++ Collections AND Generics/C02-Initial/Models/Common/Operators.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Common;
+
+public static class Operators
+{
+    public static IEnumerable<T> Once<T>(this IEnumerable<T> sequence) =>
+        new SinglePassSequence<T>(sequence);
+}
