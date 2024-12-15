bc01611eb3f1249449ef3305d5ba408443adbac2
M16A4PAPER <m16a4paper@gmail.com>
2024-12-15 17:54:56 +0500

    Inventing Shuffling Algorithm

---
+++ Collections AND Generics/C02-Initial/ConsoleDemo/Operators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    internal static class Operators
+    {
+        public static void WriteLines(this IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Collections AND Generics/C02-Initial/Models/Common/SequenceShuffle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Common
+{
+    internal class SequenceShuffle<T> : IEnumerator<T>
+    {
+        public SequenceShuffle(IEnumerable<T> sequence)
+        {
+            this.Data = sequence.ToArray();
+        }
+
+        public T Current => throw new NotImplementedException();
+
+        private T[] Data { get; }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
